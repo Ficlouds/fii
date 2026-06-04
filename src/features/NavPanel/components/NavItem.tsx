4ca86a3f484c0a3cd583e5f7427f6a47408d138c
Nad <nadheemscientist22@gmail.com>
2026-06-05 03:22:16 +0530

    fix: increase left padding for all sidebar nav items

--- src/features/NavPanel/components/NavItem.tsx
+++ src/features/NavPanel/components/NavItem.tsx
@@ -99,7 +99,7 @@ const NavItem = memo<NavItemProps>(
         clickable={!disabled}
         gap={8}
         height={36}
-        paddingInline={4}
+        paddingInline={12}
         variant={variant}
         onClick={(e) => {
           // Always prevent default <a> navigation for normal clicks to avoid full page reload.

31b3ef92ef40034f0085bcebf42dd3f6878ceb25
Nad <nadheemscientist22@gmail.com>
2026-06-05 15:55:44 +0530

    fix: white logo in incognito and dark mode

--- src/routes/(main)/home/features/index.tsx
+++ src/routes/(main)/home/features/index.tsx
@@ -195,7 +195,7 @@ const Home = memo(() => {
       <div style={{ display: 'flex', flex: 1, flexDirection: 'column', height: '100%', overflow: 'hidden', position: 'relative', width: '100%' }}>
         <div style={{ alignItems: 'center', display: 'flex', flexDirection: 'column', margin: '0 auto', maxWidth: 860, marginTop: 'auto', marginBottom: 'auto', paddingInline: 20, paddingBlock: 60, width: '100%' }}>
         {/* Fi logo */}
-        <div style={{ marginBottom: 28, textAlign: 'center', userSelect: 'none' }}><img src="/logos/fi-icon.svg" alt="Fi" style={{ height: 72, width: 'auto' }} /></div>
+        <div style={{ marginBottom: 28, textAlign: 'center', userSelect: 'none' }}><img src={isDark || incognito ? '/logos/fi-icon-white.svg' : '/logos/fi-icon-black.svg'} alt="Fi" style={{ height: 72, width: 'auto' }} /></div>
 
         {/* Input */}
         <div style={{ width: '100%', maxWidth: MAX_WIDTH, paddingInline: 20, position: 'relative' }}>

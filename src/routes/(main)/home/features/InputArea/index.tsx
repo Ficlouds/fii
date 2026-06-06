dbe1d10b6fd3131c037e2e9b3766da07c369f89c
Nad <nadheemscientist22@gmail.com>
2026-06-06 13:04:29 +0530

    fix: input padding option 1

--- src/routes/(main)/home/features/InputArea/index.tsx
+++ src/routes/(main)/home/features/InputArea/index.tsx
@@ -39,7 +39,7 @@ const InputArea = ({ incognito = false }: InputAreaProps) => {
         borderRadius: 32,
         boxShadow: 'none',
         color: isDark ? '#ececec' : '#111111',
-        minHeight: 60,
+        minHeight: 52,
         transition: 'background 0.25s ease, color 0.25s ease',
         width: '100%',
       },
